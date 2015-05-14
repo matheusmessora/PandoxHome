57e02bb3be96456d19d6c5cdbeae1c76b4745e01
Matheus Messora <tqi_mmessora@uolinc.com>
2015-05-13 22:38:48 -0300

    Themes

--- src/resources/js/app/pandox.js
+++ src/resources/js/app/pandox.js
@@ -9,7 +9,7 @@ PANDOX.SYSTEM = function () {
         console.info("opa");
         $(".main").onepage_scroll({
             sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-            easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+            easing: "ease-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                              // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
             animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
             pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
